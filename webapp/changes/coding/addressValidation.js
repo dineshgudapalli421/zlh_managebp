e6b243d8c3cbf2b85c6c0d08fb2b74bb4f08d42a
DineshGudapalli <gudapallid@londonhydro.com>
2025-09-22 19:04:35 +0530

    Display the validation response in dialog

--- webapp/changes/coding/addressValidation.js
+++ webapp/changes/coding/addressValidation.js
@@ -29,63 +29,117 @@ sap.ui.define(
                     let manageBPPoBoxLobby = sap.ui.getCore().byId("mdm.md.businesspartner.manage::sap.suite.ui.generic.template.ObjectPage.view.Details::C_BusinessPartner--to_BusinessPartnerAddrFilter::com.sap.vocabularies.UI.v1.FieldGroup::stdaddress3::POBoxPostalCode::Field-input");
                     return new Promise((resolve, reject) => {
                         debugger;
-                        if (!manageBPStreet?.getValue()) {
-                            MessageBox.error("Street is not Empty.");
-                            reject;
-                        } else if (!manageBPPostalCode?.getValue()) {
-                            MessageBox.error("Postal Code is not Empty.");
-                            reject;
-                        } else if (!manageBPCity?.getValue()) {
-                            MessageBox.error("City is not Empty.");
-                            reject;
-                        } else if (!manageBPRegion?.getValue()) {
-                            MessageBox.error("Region is not Empty.");
-                            reject;
-                        } else if (!manageBPHouseNo?.getValue()) {
-                            MessageBox.error("House No is not Empty.");
-                            reject;
-                        } else {
-                            debugger;
-                            let manageRegion = manageBPRegion?.getValue();
-                            const oStreet = manageBPStreet?.getValue();
-                            const oPostalCode = manageBPPostalCode?.getValue();
-                            const oRegion = manageRegion.match(/\((.*?)\)/);
-                            const oCity = manageBPCity?.getValue();
-                            const oHouseNo = manageBPHouseNo?.getValue();
-                            const oPostBox = manageBPPostBox?.getValue();
-                            const oPoBoxLobby = manageBPPoBoxLobby?.getValue();
+                        // if (!manageBPRegion?.getValue()) {
+                        //     MessageBox.error("Address is incomplete. Please enter country/region");
+                        //     reject;
+                        // }
+                        // else if (!manageBPStreet?.getValue() && !manageBPCity?.getValue() && !manageBPPostalCode?.getValue()) {
+                        //     MessageBox.error("Please enter Street, City, Region and Postal Code to validate the address via Street Sweeper.");
+                        //     reject;
+                        // }
+                        // else if (!manageBPStreet?.getValue() && manageBPCity?.getValue() && manageBPPostalCode?.getValue()) {
+                        //     MessageBox.error("Please enter Street to validate the address via Street Sweeper.");
+                        //     reject;
+                        // }
+                        // } else if (!manageBPPostalCode?.getValue()) {
+                        //     MessageBox.error("Postal Code is not Empty.");
+                        //     reject;
+                        // } else if (!manageBPCity?.getValue()) {
+                        //     MessageBox.error("City is not Empty.");
+                        //     reject;
+                        // } else if (!manageBPRegion?.getValue()) {
+                        //     MessageBox.error("Region is not Empty.");
+                        //     reject;
+                        // } else if (!manageBPHouseNo?.getValue()) {
+                        //     MessageBox.error("House No is not Empty.");
+                        //     reject;
+                        // } 
+                        // else {
+                        debugger;
+                        let manageRegion = manageBPRegion?.getValue();
+                        const oStreet = manageBPStreet?.getValue();
+                        const oPostalCode = manageBPPostalCode?.getValue();
+                        const oRegion = (manageRegion === '' || manageRegion === undefined) ? '' : manageRegion.match(/\((.*?)\)/);
+                        const oCity = manageBPCity?.getValue();
+                        const oHouseNo = manageBPHouseNo?.getValue();
+                        const oPostBox = manageBPPostBox?.getValue();
+                        const oPoBoxLobby = manageBPPoBoxLobby?.getValue();
 
-                            var oModel = this.getView().getModel("customer.oData");
-                            oModel.callFunction("/AddressValidation", {
-                                method: "GET",
-                                urlParameters: {
-                                    City: oCity,
-                                    PostalCode: oPostalCode,
-                                    Region: oRegion[1],
-                                    Street: oStreet,
-                                    HouseNo: oHouseNo,
-                                    PoBox: oPostBox,
-                                    PoBoxLobby: oPoBoxLobby
-                                },
-                                success: function (response) {
-                                    debugger;
-                                    if (response.AddressValidation.Correction === "") {
-                                        MessageBox.success(response.AddressValidation.Result);
+                        var oModel = this.getView().getModel("customer.oData");
+                        oModel.callFunction("/AddressValidation", {
+                            method: "GET",
+                            urlParameters: {
+                                City: oCity,
+                                PostalCode: oPostalCode,
+                                Region: (manageRegion === '' || manageRegion === undefined) ? '' : oRegion[1],
+                                Street: oStreet,
+                                HouseNo: oHouseNo,
+                                PoBox: oPostBox,
+                                PoBoxLobby: oPoBoxLobby
+                            },
+                            success: function (response) {
+                                debugger;
+                                let oProposed = response.AddressValidation.Proposed;
+                                oProposed = oProposed.replaceAll('<br>', '\n');
+                                var sTableId = "dynamicTable_" + new Date().getTime();
+                                var oTable = new sap.m.Table(sTableId, {
+                                    mode: sap.m.ListMode.Single,
+                                    inset: false,
+                                    showOverlay: false,
+                                    headerDesign: sap.m.ListHeaderDesign.Standard
+                                });
+
+                                var oModel = new sap.ui.model.json.JSONModel();
+                                oTable.setModel(oModel);
+                                var aColumnData = [
+                                    { id: "col1", text: "Result" },
+                                    { id: "col2", text: "Correction" },
+                                    { id: "col3", text: "Proposed" }
+                                ];
+
+                                aColumnData.forEach(function (oCol) {
+                                    oTable.addColumn(new sap.m.Column({
+                                        header: new sap.m.Label({ text: oCol.text })
+                                    }));
+                                });
+                                var oSingleRowData = {
+                                    col1: response.AddressValidation.Result,
+                                    col2: response.AddressValidation.Correction,
+                                    col3: oProposed
+                                };
+                                oModel.setData({
+                                    rows: [oSingleRowData] // Wrap the single row data in an array
+                                });
+
+                                oTable.bindItems("/rows", new sap.m.ColumnListItem({
+                                    cells: [
+                                        new sap.m.Text({ text: "{col1}" }),
+                                        new sap.m.Text({ text: "{col2}" }),
+                                        new sap.m.Text({ text: "{col3}" })
+                                    ]
+                                }));
+                                var oDialog = new sap.m.Dialog({
+                                    title: "Information", //Available Documents
+                                    closed: function (oEvent) {
+                                        oTable.destroy();
+                                        oEvent.getSource().destroy();
                                         resolve;
                                     }
-                                    else if (response.AddressValidation.Correction !== "") {
-                                        MessageBox.error(response.AddressValidation.Correction);
-                                        reject;
-                                    }
+                                });
 
-                                },
-                                error: (oError) => {
-                                    debugger;
-                                    MessageBox.error(JSON.parse(oError.responseText).error.message.value);
-                                    reject;
-                                }
-                            });
-                        }
+                                oDialog.addContent(oTable);
+                                oDialog.addButton(new sap.m.Button({ text: "Ok", press: function () { oDialog.close(); } }));				//Button for printing
+                                oDialog.addButton(new sap.m.Button({ text: "Cancel", press: function () { oDialog.close(); } }));	//Button for closing Dialog
+                                oDialog.open();
+                                resolve;
+                            },
+                            error: (oError) => {
+                                debugger;
+                                MessageBox.error(JSON.parse(oError.responseText).error.message.value);
+                                reject;
+                            }
+                        });
+                        // }
                     });
                 }
             }
